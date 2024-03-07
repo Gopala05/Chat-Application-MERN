ec5ae6b70f6cb1fb855af852f795b12feca01d91
Gopala05 <gopigopala05vkbrg@gmail.com>
2024-03-07 21:26:29 +0530

    User Modal

---
+++ backend/models/user.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const userScheme = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 4
+    },
+    gender: {
+        type: String,
+        required: true,
+        enum:['male', 'female']
+    },
+    profileImage: {
+        type: String,
+        default: "",
+    }
+});
+
+const UserModal = mongoose.model("User", userScheme);
+
+export default UserModal;
