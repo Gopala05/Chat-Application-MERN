cef7e512a7a224dc13641e88b9633dee0fab900e
Gopala05 <gopigopala05vkbrg@gmail.com>
2024-03-07 21:28:25 +0530

    SignUp Update

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,16 +1,55 @@
+import UserModal from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+
+//Sign Up
 export const signup = async (req, res) => {
-    try {
-        const { fullName, userName, password, confirmPassword, gender }= req.body;
-    } catch (error) {
-        
+  try {
+    const { fullName, userName, password, confirmPassword, gender } = req.body; //Payload
+
+    if (password !== confirmPassword) {
+      return res.status(400).json({ error: "Password don't match" });
     }
-}
 
+    const userExist = await UserModal.findOne({ userName });
+
+    if (userExist) {
+      return res.status(400).json({ error: "User already Exist" });
+    }
+
+    //Hashing the Password
+
+    //Random Profile Pic API
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${userName}`;
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${userName}`;
+
+    const newUser = new UserModal({
+      fullName,
+      username: userName, //Since the "username" in Modal is not Same as "userName"
+      password,
+      gender,
+      profileImage: gender === "male" ? boyProfilePic : girlProfilePic,
+    });
+
+    await newUser.save(); //Save User
+
+    res.status(201).json({
+      _id: newUser.id, //Mongo Creates ID by default
+      fullName: newUser.fullName,
+      username: newUser.username,
+      profilePic: newUser.profileImage,
+    });
+  } catch (error) {
+    console.log("Sign Up Error: ", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+//Log-In
 export const login = (req, res) => {
-    console.log("User Login");
-}
+  console.log("User Login");
+};
 
+//Log-Out
 export const logout = (req, res) => {
-    console.log("User Logout");
-}
-
+  console.log("User Logout");
+};
