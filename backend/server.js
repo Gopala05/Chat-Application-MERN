724982066fe27ba20d4c1e77c8eb97f187df1478
Gopala05 <gopigopala05vkbrg@gmail.com>
2024-03-08 18:23:00 +0530

    Server.js Update

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
 
-import authRoute from "./routes/auth.routes.js"
+import authRoute from "./routes/auth.routes.js";
 import connection from "./db/connectTOMongoDB.js";
 
 const app = express();
-
-dotenv.config()
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-    res.send("OK GK")
+dotenv.config();
+
+//Middleware
+app.use(express.json());
+app.use("/api/auth", authRoute);
+
+// app.get("/", (req, res) => {
+//   res.send("OK GK");
+// });
+
+app.listen(PORT, () => {
+  connection(); 
+  console.log(`Server running on port ${PORT}`);
 });
-
-app.use("/api/auth", authRoute)
-
-app.listen(PORT, () => { connection(), console.log(`Server running on port ${PORT}`)})
