6830a9a233b5a52f8dbf02d0f4f91102a7035144
Gopala05 <gopigopala05vkbrg@gmail.com>
2024-04-12 19:30:31 +0530

    User Model JS Update

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,29 +1,29 @@
 import mongoose from "mongoose";
 
 const userScheme = new mongoose.Schema({
-    fullName: {
-        type: String,
-        required: true
-    },
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 4
-    },
-    gender: {
-        type: String,
-        required: true,
-        enum:['male', 'female']
-    },
-    profileImage: {
-        type: String,
-        default: "",
-    }
+  fullName: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 4,
+  },
+  gender: {
+    type: String,
+    required: true,
+    enum: ["male", "female"],
+  },
+  profileImage: {
+    type: String,
+    default: "",
+  },
 });
 
 const UserModal = mongoose.model("User", userScheme);
