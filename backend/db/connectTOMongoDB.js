392884e77bfa0de7e868e7319c81a521a4c77651
Gopala05 <gopigopala05vkbrg@gmail.com>
2024-04-17 20:33:26 +0530

    Connectiom Update

--- backend/db/connectTOMongoDB.js
+++ backend/db/connectTOMongoDB.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
 const connection = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_DB_URL);
-        console.log("Connection Successfull");
-    }catch (error) {
-        console.log("MongoDB Connection Error: ", error.message)
-    }
-}
+  try {
+    await mongoose.connect(process.env.MONGO_DB_URL);
+    console.log("Connection Successfull");
+  } catch (error) {
+    console.log("MongoDB Connection Error: ", error.message);
+  }
+};
 
 export default connection;
