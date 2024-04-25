89d5bc210773352c8f41879033285634cd3dcedf
Gopala05 <gopigopala05vkbrg@gmail.com>
2024-04-25 21:33:03 +0530

    Message Controller

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,8 +1,16 @@
-export const sendingMessage = async (req, res) => {
-    try {
+import ConversationModal from "../models/conversation.model.js";
 
-    } catch (error) {
-        console.log("Error in Sending Message: ",error.message)
-        res.status(500).json({error: "Internal Server Error"});
-    }
-}
+export const sendingMessage = async (req, res) => {
+  try {
+    const { message } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
+
+    let conversation = ConversationModal.findOne({
+      participants: { $all: [senderId, receiverId] },
+    });
+  } catch (error) {
+    console.log("Error in Sending Message: ", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
