08846ec9ddbc826567574c8eaa6e79a8be71bf82
Gopala05 <gopigopala05vkbrg@gmail.com>
2024-05-07 16:14:29 +0530

    Messages Model and Route Update

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,4 +1,5 @@
 import ConversationModal from "../models/conversation.model.js";
+import MessageModal from "../models/message.model.js";
 
 export const sendingMessage = async (req, res) => {
   try {
@@ -6,11 +7,53 @@ export const sendingMessage = async (req, res) => {
     const { id: receiverId } = req.params;
     const senderId = req.user._id;
 
-    let conversation = ConversationModal.findOne({
+    let conversation = await ConversationModal.findOne({
       participants: { $all: [senderId, receiverId] },
     });
+
+    if (!conversation) {
+      conversation = await ConversationModal.create({
+        participants: [senderId, receiverId],
+      });
+    }
+
+    const newMessage = new MessageModal({
+      senderId,
+      receiverId,
+      message,
+    });
+
+    if (newMessage) {
+      conversation.messages.push(newMessage._id);
+    }
+    // await conversation.save();
+    // await newMessage.save();
+
+    //The below statements of saving in Conversation and Messages will run in Parallel
+    await Promise.all([conversation.save(), newMessage.save()]);
+
+    res.status(201).json(newMessage);
   } catch (error) {
     console.log("Error in Sending Message: ", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: ChattingUserID } = req.params;
+    const senderId = req.user._id;
+    const conversation = await ConversationModal.findOne({
+      participants: { $all: [senderId, ChattingUserID] },
+    }).populate("messages"); //Return or Populate the messages in "messages" table or model
+
+    if (!conversation) return res.status(200).json([]);
+
+    const messages = conversation.messages;
+
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log("Error in Getting the Message: ", error.message);
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { sendingMessage } from "../controllers/message.controller.js";
+import {
+  getMessages,
+  sendingMessage,
+} from "../controllers/message.controller.js";
 import protectRoute from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
+router.get("/:id", protectRoute, getMessages);
 router.post("/send/:id", protectRoute, sendingMessage);
 
 export default router;
