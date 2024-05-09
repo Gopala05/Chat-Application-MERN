ca10dc4627ce9b7f2327028d13afc07f606838e4
Gopala05 <gopigopala05vkbrg@gmail.com>
2024-05-10 01:58:27 +0530

    Hooks Update

---
+++ frontend/src/hooks/useSignUp.js
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useAuthContext } from "../components/context/AuthContext";
+
+const useSignUp = () => {
+  const [loading, setLoading] = useState(false);
+  const { setUser } = useAuthContext();
+
+  const SignUp = async ({
+    fullName,
+    username,
+    password,
+    confirmPassword,
+    gender,
+  }) => {
+    console.log({
+      fullName,
+      username,
+      password,
+      confirmPassword,
+      gender,
+    });
+    const isValid = handleValidation({
+      fullName,
+      username,
+      password,
+      confirmPassword,
+      gender,
+    });
+    if (!isValid) return;
+
+    setLoading(true);
+    try {
+      const payload = JSON.stringify({
+        fullName,
+        username,
+        password,
+        confirmPassword,
+        gender,
+      });
+      const response = await axios.post("/api/auth/signup", payload, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const data = response.data;
+
+      if (data.error) {
+        throw new Error(data.error);
+      }
+
+      //Update Local Storage
+      localStorage.setItem("user-info", JSON.stringify(data));
+
+      //Context setUser
+      setUser(data);
+
+    } catch (error) {
+      toast.error(error.response.data.error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { loading, SignUp };
+};
+
+export default useSignUp;
+
+function handleValidation({
+  fullName,
+  username,
+  password,
+  confirmPassword,
+  gender,
+}) {
+  if (!fullName || !username || !password || !confirmPassword || !gender) {
+    toast.error("Please fill in all tyhe fields to Sign Up");
+    return false;
+  }
+
+  if (password !== confirmPassword) {
+    toast.error("Password and Confirm Password do not match");
+    return false;
+  }
+
+  if (password.length < 4) {
+    toast.error("Password must be at least 4 characters long.");
+    return false;
+  }
+
+  return true;
+}
--- frontend/src/pages/signup/GenderCheckBox.jsx
+++ frontend/src/pages/signup/GenderCheckBox.jsx
@@ -1,30 +1,57 @@
-import React from 'react'
+import React from "react";
 
-const GenderCheckBox = () => {
+const GenderCheckBox = ({ onCheckBoxChange, selectedGender }) => {
   return (
-    <div className='flex mt-2'>
-        <div className='form-control'>
-            <label className='label gap-2 cursor-pointer'>
-                <span className='label-text text-black font-extrabold'>Male</span>
-                <input type="checkbox" className='checkbox checkbox-accent' />
-            </label>
-        </div>
+    <div className="flex mt-2">
+      <div className="form-control">
+        <label
+          className={`label gap-2 cursor-pointer ${
+            selectedGender === "male" ? "selected" : ""
+          }`}
+        >
+          <span className="label-text text-black font-extrabold">Male</span>
+          <input
+            type="checkbox"
+            className="checkbox checkbox-accent"
+            checked={selectedGender === "male"}
+            onChange={() => onCheckBoxChange("male")}
+          />
+        </label>
+      </div>
 
-        <div className='form-control'>
-            <label className='label gap-2 cursor-pointer'>
-                <span className='label-text text-black font-extrabold'>Female</span>
-                <input type="checkbox" className='checkbox checkbox-accent' />
-            </label>
-        </div>
+      <div className="form-control">
+        <label
+          className={`label gap-2 cursor-pointer ${
+            selectedGender === "female" ? "selected" : ""
+          }`}
+        >
+          <span className="label-text text-black font-extrabold">Female</span>
+          <input
+            type="checkbox"
+            className="checkbox checkbox-accent"
+            checked={selectedGender === "female"}
+            onChange={() => onCheckBoxChange("female")}
+          />
+        </label>
+      </div>
 
-        <div className='form-control'>
-            <label className='label gap-2 cursor-pointer'>
-                <span className='label-text text-black font-extrabold'>Others</span>
-                <input type="checkbox" className='checkbox checkbox-accent' />
-            </label>
-        </div>
+      <div className="form-control">
+        <label
+          className={`label gap-2 cursor-pointer ${
+            selectedGender === "others" ? "selected" : ""
+          }`}
+        >
+          <span className="label-text text-black font-extrabold">Others</span>
+          <input
+            type="checkbox"
+            className="checkbox checkbox-accent"
+            checked={selectedGender === "others"}
+            onChange={() => onCheckBoxChange("others")}
+          />
+        </label>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default GenderCheckBox
+export default GenderCheckBox;
