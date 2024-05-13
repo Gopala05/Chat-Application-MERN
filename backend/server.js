4eaaebcdd83462b7724f98b00bdd48aed32e8afb
Gopala05 <gopigopala05vkbrg@gmail.com>
2024-05-13 17:43:43 +0530

    Backend Server Update to redirect to Frontend

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import authRoute from "./routes/auth.routes.js";
 import messageRoute from "./routes/message.routes.js";
 import userRoute from "./routes/user.routes.js";
+import { app, server } from "./socket/socket.js";
 
 import connection from "./db/connectTOMongoDB.js";
 import cors from "cors";
 
-const app = express();
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -25,11 +28,13 @@ app.use("/api/auth", authRoute);
 app.use("/api/message", messageRoute);
 app.use("/api/users", userRoute);
 
-// app.get("/", (req, res) => {
-//   res.send("OK GK");
-// });
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.listen(PORT, () => {
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/frontend/dist/index.html"));
+});
+
+server.listen(PORT, () => {
   connection();
   console.log(`Server running on port ${PORT}`);
 });
